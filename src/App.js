4e1029c3a9185affa653ec053045b3bc5365ab7f
fidelanders <fidelanders@gmail.com>
2022-02-28 13:00:12 +0100

    update

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import "./App.css";
+import About from "./About";
+import Blog from "./Blog";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Home from "./Home";
+// import Project from "./Project";
+// import Service from "./Service";
+
+function App() {
+  return (
+    <div className="App">
+       <Home />
+       <About />
+       <Blog />
+       <Contact />
+       <Footer />
+      {/*  <Service />
+       <Project />
+       
+       
+        */}
+    </div>
+  );
+}
+
+export default App;
